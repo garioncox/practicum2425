7bfc98dc1e8e27155f508e9f193c1b04e0181450
Bryce Coon <bryce.coon@students.snow.edu>
2024-09-10 17:56:42 -0600

    [KGB-26] connected server to db

---
+++ practicum2425.Server/Services/CompanyService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using practicum2425.Server.Data;
+
+namespace practicum2425.Server.Services;
+
+public interface ICompanyService
+{
+    public Task<List<Company>> GetCompanyListAsync();
+}
+
+
+public class CompanyService : ICompanyService
+{
+    readonly PostgresContext _context;
+    public CompanyService(PostgresContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Company>> GetCompanyListAsync()
+    {
+        return await _context.Companies.ToListAsync();
+    }
+}
