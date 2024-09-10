0aef37bb83aec3b992b7139fb498061c39a7f911
Garion Cox <garion.cox@students.snow.edu>
2024-09-10 16:47:16 -0600

    [KGB-27] Added nUnit testing framework

---
+++ practicum2425.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace practicum2425.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
