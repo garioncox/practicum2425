49036912ec42167dfd3f1a11a7673b5ca94ee81f
Garion Cox <garion.cox@students.snow.edu>
2024-09-11 17:38:04 -0600

    [KGB-32] Can display database info on the front end

--- practicum2425.Server/Controllers/CompanyController.cs
+++ practicum2425.Server/Controllers/CompanyController.cs
@@ -2,22 +2,21 @@
 using practicum2425.Server.Data;
 using practicum2425.Server.Services;
 
-namespace practicum2425.Server.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class CompanyController : ControllerBase
-    {
-        private readonly ICompanyService _companyService;
-        public CompanyController(ICompanyService service)
-        {
-            _companyService = service;
-        }
+namespace practicum2425.Server.Controllers;
 
-        [HttpGet("GetCompanies")]
-        public async Task<List<Company>> GetCompanyListAsync()
-        {
-            return await _companyService.GetCompanyListAsync();
-        }
+[ApiController]
+[Route("api/[controller]")]
+public class CompanyController : ControllerBase
+{
+    private readonly ICompanyService _companyService;
+    public CompanyController(ICompanyService service)
+    {
+        _companyService = service;
+    }
+
+    [HttpGet("GetCompanies")]
+    public async Task<List<Company>> GetCompanyListAsync()
+    {
+        return await _companyService.GetCompanyListAsync();
     }
 }
