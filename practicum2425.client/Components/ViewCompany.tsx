5972f42b5f6027c40afc87b2eb93e9424d860077
Bryce Coon <bryce.coon@students.snow.edu>
2024-09-13 17:19:07 -0600

    Refactored, took out company into its own component

---
+++ practicum2425.client/Components/ViewCompany.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Company } from '../DataInterface/CompanyInterface';
+import React from 'react';
+
+function ViewProject() {
+    const [companies, setCompanies] = useState<Company[]>();
+
+    useEffect(() => {
+        populateCompanyData();
+    }, []);
+
+    async function populateCompanyData() {
+        const response = await fetch('/api/Company/GetCompanies');
+        const data = await response.json();
+        setCompanies(data);
+    }
+
+    const contents = companies === undefined ? (<p>Loading... Please refresh once the ASP.NET backend has started.</p>) :
+        (
+            <div>
+                {companies.map(c =>
+                    <p key={c.id}>{c.id} {c.name}</p>
+                )}
+            </div>
+        )
+
+    return (
+        <div>
+            <h1 id="tableLabel">Company List</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+        </div>
+    )
+
+};
+
+export default ViewProject
