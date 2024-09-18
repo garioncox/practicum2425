5d2dd54320f2c9df881509de053ebb1551ec3ab8
Bryce Coon <bryce.coon@students.snow.edu>
2024-09-17 18:41:16 -0600

    [KBG-52] added oAuth

--- practicum2425.client/src/App.tsx
+++ practicum2425.client/src/App.tsx
@@ -5,20 +5,40 @@ import ProjectForm from '../Components/ProjectForm';
 import ViewProject from '../Components/ViewProject';
 import { Shift } from '../DataInterface/ShiftInterface';
 import ShiftForm from '../Components/ShiftForm'
+import { Auth0Provider } from '@auth0/auth0-react';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
+import Profile from './components/profile';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
     const [shifts, setShifts] = useState<Shift[]>()
-
     return (
         <>
-            <ViewCompany />
-            <ViewShift
-                shifts={shifts}
-                setShifts={setShifts}
-            />
-            <ViewProject />
-            <ProjectForm />
-            <ShiftForm />
+            {/* <BrowserRouter> */}
+                <Auth0Provider
+                    domain="dev-zas6rizyxopiwv2b.us.auth0.com"
+                    clientId="BOZHiKTbFJOrquI2E4QMI2qARqMW9OgC"
+                    authorizationParams={{
+                        redirect_uri: window.location.origin
+                    }}
+                >
+
+                    <LoginButton></LoginButton>
+                    <LogoutButton></LogoutButton>
+                    <Profile></Profile>
+                    
+
+                    <ViewCompany />
+                    <ViewShift
+                        shifts={shifts}
+                        setShifts={setShifts}
+                    />
+                    <ViewProject />
+                    <ProjectForm />
+                    <ShiftForm />
+                </Auth0Provider>
+                {/* </BrowserRouter> */}
         </>
     );
 }
