58afc79baca71ed41d5fbf251d056f96a71753ac
brycecoon <112597142+brycecoon@users.noreply.github.com>
2024-09-19 16:29:02 -0600

    login fully works

--- practicum2425.client/Components/Navbar.tsx
+++ practicum2425.client/Components/Navbar.tsx
@@ -1,49 +1,74 @@
-import { Auth0Provider } from '@auth0/auth0-react';
-import LoginButton from '../src/components/login';
-import LogoutButton from '../src/components/logout';
-import Profile from '../src/components/profile';
-import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import LoginButton from "../src/components/login";
+import LogoutButton from "../src/components/logout";
+import Profile from "../src/components/profile";
 
 function Navbar() {
-    return (
-        <>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary py-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <Profile></Profile>
+
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+
             <Auth0Provider
-                domain="dev-zas6rizyxopiwv2b.us.auth0.com"
-                clientId="BOZHiKTbFJOrquI2E4QMI2qARqMW9OgC"
-                authorizationParams={{
-                    redirect_uri: window.location.origin
-                }}
+              domain="dev-zas6rizyxopiwv2b.us.auth0.com"
+              clientId="BOZHiKTbFJOrquI2E4QMI2qARqMW9OgC"
+              authorizationParams={{
+                redirect_uri: window.location.origin,
+              }}
             >
-                <LoginButton></LoginButton>
-                <LogoutButton></LogoutButton>
-                <Profile></Profile>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary py-3">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Features</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Pricing</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+              <ul className="ms-auto navbar-nav">
+                <li className="nav-item mx-2">
+                  <LoginButton />
+                </li>
+                <li className="nav-item mx-2">
+                  <LogoutButton />
+                </li>
+              </ul>
             </Auth0Provider>
-        </>
-    );
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
