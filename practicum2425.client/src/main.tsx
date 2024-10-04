ce4e22d57e5c300f8a31b248f5456921f3bbb124
Garion Cox <garion.cox@students.snow.edu>
2024-10-04 16:27:38 -0600

    Extracted duplicate auth0context into main.tsx

--- practicum2425.client/src/main.tsx
+++ practicum2425.client/src/main.tsx
@@ -1,31 +1,42 @@
-import { StrictMode } from 'react'
-import App from './App.tsx'
-import Navbar from './Components/Navbar.tsx'
-import './index.css'
-import Sidebar from './Components/Sidebar.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom'
+import { StrictMode } from "react";
+import App from "./App.tsx";
+import Navbar from "./Components/Navbar.tsx";
+import "./index.css";
+import Sidebar from "./Components/Sidebar.tsx";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <StrictMode>
-            <div className="text-center vh-100">
-                <div className="row h-100">
-                    {/*Sidebar*/}
-                    <div className="col-2 bg-dark text-light py-5 d-flex justify-content-center">
-                        <Sidebar />
-                    </div>
-
-                    {/*Main layout*/}
-                    <div className="col-10 px-0">
-                        <Navbar />
-                        <div className="m-5">
-                            <App />
-                        </div>
-                    </div>
-                </div>
+  <BrowserRouter>
+    <StrictMode>
+      <Auth0Provider
+        domain="dev-zas6rizyxopiwv2b.us.auth0.com"
+        clientId="BOZHiKTbFJOrquI2E4QMI2qARqMW9OgC"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+        cacheLocation="localstorage"
+      >
+        <div className="text-center vh-100">
+          <div className="row h-100">
+            {/*Sidebar*/}
+            <div className="col-2 bg-dark text-light py-5 d-flex justify-content-center">
+              <Sidebar />
             </div>
-        </StrictMode>,
-    </BrowserRouter>,
-    document.getElementById("root")
-)
+
+            {/*Main layout*/}
+            <div className="col-10 px-0">
+              <Navbar />
+              <div className="m-5">
+                <App />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Auth0Provider>
+    </StrictMode>
+    ,
+  </BrowserRouter>,
+  document.getElementById("root")
+);
