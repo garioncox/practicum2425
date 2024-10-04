1180ed9f4a7125e060ad9da845134e0fe1637efa
kalin.nebeker <kalin.nebeker@ENGR9HCP5S3>
2024-10-04 17:34:12 -0600

    didn't save

--- practicum2425.client/src/Pages/EmployeeDetails.tsx
+++ practicum2425.client/src/Pages/EmployeeDetails.tsx
@@ -4,72 +4,72 @@ import { Employee } from '../Data/Interfaces/EmployeeInterface'
 import { Shift } from '../Data/Interfaces/Shift'
 
 function EmployeeDetails() {
-    const { id } = useParams()
-    const [employee, setEmployees] = useState<Employee>()
-    const [shift, setShift] = useState<Shift[]>()
+  const { id } = useParams()
+  const [employee, setEmployees] = useState<Employee>()
+  const [shift, setShift] = useState<Shift[]>()
 
-    useEffect(() => {
-        getEmploye()
-    }, [])
+  useEffect(() => {
+    getEmploye()
+  }, [])
 
-    async function getEmploye() {
-        const responseEmployee = await fetch(import.meta.env.VITE_API_URL + `api/Employee/get/${id}`);
-        const responseShift = await fetch(import.meta.env.VITE_API_URL + `api/EmployeeShift/getShifts/${id}`);
-        const empVal = await responseEmployee.json()
-        const shiftVal = await responseShift.json()
+  async function getEmploye() {
+    const responseEmployee = await fetch(import.meta.env.VITE_API_URL + `api/Employee/get/${id}`);
+    const responseShift = await fetch(import.meta.env.VITE_API_URL + `api/EmployeeShift/getShifts/${id}`);
+    const empVal = await responseEmployee.json()
+    const shiftVal = await responseShift.json()
 
-        setEmployees(empVal)
-        setShift(shiftVal)
-    }
+    setEmployees(empVal)
+    setShift(shiftVal)
+  }
 
-    function employeeDetails() {
-        return (employee != undefined ?
-            <>
-                <h1>{employee?.name}</h1>
-                <h2>{employee.email}</h2>
-                <h2>{employee.phonenumber}</h2>
-            </>
-            : <div>No employee found with that Id</div>)
-    }
+  function employeeDetails() {
+    return (employee != undefined ?
+      <>
+        <h1>{employee?.name}</h1>
+        <h2>{employee.email}</h2>
+        <h2>{employee.phonenumber}</h2>
+      </>
+      : <div>No employee found with that Id</div>)
+  }
 
-    function shiftDetails() {
-        return (
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th className="text-start">Shift Details</th>
-                        <th className="text-start">Location</th>
-                        <th className="text-start">Start Time</th>
-                        <th className="text-start">End Time</th>
-                        <th className="text-start">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {shift?.map((s) =>
-                        <tr>
-                            <td className="text-start">{s.description}</td>
-                            <td className="text-start">{s.location}</td>
-                            <td className="text-start">{s.startTime}</td>
-                            <td className="text-start">{s.startTime}</td>
-                            <td className="text-start">{s.status}</td>
-
-                        </tr>)}
-                </tbody>
-            </table>
-        )
-    }
-
+  function shiftDetails() {
     return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th className="text-start">Shift Details</th>
+            <th className="text-start">Location</th>
+            <th className="text-start">Start Time</th>
+            <th className="text-start">End Time</th>
+            <th className="text-start">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shift?.map((s) =>
+            <tr>
+              <td className="text-start">{s.description}</td>
+              <td className="text-start">{s.location}</td>
+              <td className="text-start">{s.startTime}</td>
+              <td className="text-start">{s.startTime}</td>
+              <td className="text-start">{s.status}</td>
 
-        <>
-            <div>
-                {employeeDetails()}
-            </div>
-
-            <div>
-                {shiftDetails()}
-            </div>
-        </>
+            </tr>)}
+        </tbody>
+      </table>
     )
+  }
+
+  return (
+
+    <>
+      <div>
+        {employeeDetails()}
+      </div>
+
+      <div>
+        {shiftDetails()}
+      </div>
+    </>
+  )
 }
 export default EmployeeDetails
